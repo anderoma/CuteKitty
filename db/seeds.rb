4ef9376305d6c8c945377bfe6a39f5ed4fe38173
margauxchx <margaux.chaix@yahoo.fr>
2019-11-25 17:16:49 +0100

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,12 @@ require 'faker'
 Item.destroy_all
 ActiveRecord::Base.connection.reset_pk_sequence!('items')
 
-5.times do 
+20.times do 
   item = Item.create!(
     title: Faker::Creature::Animal.name,
     description: Faker::Lorem.paragraph_by_chars,
     price: Faker::Number.decimal(l_digits: 2),
-    image_url:[:image1,:image2,:image3,:image4])
+    image_url:["image1","image2","image3","image4"].sample)
   
 end
 
