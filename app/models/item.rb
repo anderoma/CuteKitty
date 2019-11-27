acc678fbee945cc3b3e2efa6d83e3687ff126213
margauxchx <margaux.chaix@yahoo.fr>
2019-11-27 10:05:03 +0100

    Validations item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  validates :title, presence: true,
+           length: {minimum: 2}
+  validates :description, presence: true,
+           length: {maximum: 500}
+  validates :price, presence: true
+  validates :image_url, presence: true
 end
